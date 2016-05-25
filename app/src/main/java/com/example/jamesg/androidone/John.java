16990e63187a03d31ed37f104807293cdb77c4ad
gitalry <jimgitari@gmail.com>
2016-05-25 18:27:32 +0300

    updated sys buiild

---
+++ app/src/main/java/com/example/jamesg/androidone/John.java
@@ -0,0 +1,13 @@
+package com.example.jamesg.androidone;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class John extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_john);
+    }
+}
---
+++ app/src/main/java/com/example/jamesg/androidone/What.java
@@ -0,0 +1,13 @@
+package com.example.jamesg.androidone;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class What extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_what);
+    }
+}
